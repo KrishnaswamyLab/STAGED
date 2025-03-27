76334793cfadd31d3c5775f4448a56d4ee5c0a42
Joao Felipe Rocha <joaofelipe.rocha@.yale.edu>
2025-03-27 12:27:32 -0400

    Changes in the dataset

--- src/data/components/spatial_temporal_dataset.py
+++ src/data/components/spatial_temporal_dataset.py
@@ -35,7 +35,7 @@ class SpatialTemporalDataset(Dataset):
 
     def process(self):
         """Process raw data into graph objects and save them."""
-
+        
         ##TODO correct this processing step
         raw_data = torch.load(os.path.join(self.raw_dir, "data.pt"))  # Example raw data
 
@@ -44,14 +44,35 @@ class SpatialTemporalDataset(Dataset):
         (num_cell_types, graph_priors) = prior_graphs_data.shape
         (num_cells,1) = cell_types.shape # cell types label for each cell on the dataset
 
-        print(raw_data.shape) #should delete
-        for i, graph in enumerate(raw_data):
-            data = Data(
-                x=graph["x"], 
-                edge_index=graph["edge_index"], 
-                y=graph["y"]
-            )
-            torch.save(data, os.path.join(self.processed_dir, f"graph_{i}.pt"))
+        # Initial GRN
+        ## We retrieve the GRN relative to that cell type.
+        GRN = prior_graphs_data[cell_type]
+        # Add output ligand and input receptors
+        GRN_ligand_receptors = add_ligand_receptors(spatial_data)
+
+        # Connect the GRN to its neighbors
+        ## Connect the neighbors receptors with the ligands based on spatial locations
+        neighbors - find_neighbors(spatial_data, cell_types)
+        GRN_neighbors = connect_neighbors(neighbors, GRN_ligand_receptors)
+        GRN_final = GRN_ligand_receptors + GRN_neighbors
+
+        # Assign node features to the GRN
+        ## Assign the GRN nodes with the genes expression levels based on the time lag
+        GRN_final[node_features] = retrieve_genes_expression(neighbors,GRN_final, time_lag)
+        
+        prior_GRN_features = GRN.node_features
+        time_laged_GRN_features = GRN_final.time_lagged_features
+
+        # Assign node features using time lag
+
+        # for i, graph in enumerate(raw_data):
+        #     data = Data(
+        #         x=graph["x"], 
+        #         edge_index=graph["edge_index"], 
+        #         y=graph["y"]
+        #     )
+
+        torch.save(data, os.path.join(self.processed_dir, f"graph_{i}.pt"))
 
     def len(self):
         return len(self.processed_file_names)
